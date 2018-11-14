638bab19642eaa30582f538cd25dfc9a9d25ddfb
colorful-daemon-5175 <rozmusnicholas@gmail.com>
2018-11-14 19:11:44 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,4 @@ def introduction(name)
 end
   def introduction_with_language(name, language)
     puts "Hi, my name is #{name} and I am learning to program in #{language}".
-    def (dan)
-      def(ember.js)
-end
 end
